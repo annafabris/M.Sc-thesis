ca036653c5cbe7fd27a4c25cf425f98f6091d78f
annafabris <xxanninaxx4@gmail.com>
2022-07-05 18:03:09 +0200

    Create import.py

---
+++ utilities/import.py
@@ -0,0 +1,41 @@
+import json
+import nltk
+
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+def sentences_from_txt(file_name):
+    """
+    Reads a .txt file and slit it in a list of sentences.
+
+    Parameters:
+    file_name (str): the name of the file containing the output of Margot
+
+    Returns:
+    list: the list of sentences
+    """
+
+    summary = open(file_name).read()
+    summary = tokenizer.tokenize(summary)
+    summary = [item.replace('\n', ' ') for item in summary if len(item) >= 7]
+    return summary
+
+def sentences_from_margot_json(file_name):
+    """
+    Get the arguments (claim / evidences) from the output file of Margot
+
+    Parameters:
+    file_name (str): the name of the file containing the output of Margot
+
+    Returns:
+    list: the list of sentences
+    """
+
+    claims_or_evidences = []
+
+    f = open(file_name) 
+    data = json.load(f)
+    for sentence in data['document']:
+        if((sentence['claim_score'] > 0 or sentence['evidence_score'] > 0) and len(sentence['text']) > 7):
+            claims_or_evidences.append(sentence['text'])
+    f.close()
+    return claims_or_evidences
